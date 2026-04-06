5850a64d415079229616ee404dd041acf95b73f8
hanamanajun-sudo <hanamanajun@gmail.com>
2026-04-06 20:47:45 +0900

    Add Google AdSense script to head

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6443201130119317" crossOrigin="anonymous"></script>
       </head>
       <body>
         <ReadingProgress />
